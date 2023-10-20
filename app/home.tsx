36a3fcb7b4f92b473eef84be944bef218b4472e9
blackrozez <139424853+blackrozez@users.noreply.github.com>
2023-10-20 20:07:13 +0700

    main-1

---
+++ app/home.tsx
@@ -0,0 +1,75 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="w-[1920px] h-[3148px] relative bg-white">
+  <div className="w-[1639px] h-[0px] left-[140.50px] top-[3027.50px] absolute opacity-60 border border-neutral-500"></div>
+  <div className="w-[787px] h-[161px] left-[567px] top-[3041px] absolute opacity-60 text-center text-black text-xl font-normal font-['Segoe UI'] leading-[49px]">Hak cipta © 2018 TiketQ.com Travel Pte. Ltd. All rights reserved<br/>PT. INITIKETQUE | Batam, Indonesia</div>
+  <div className="w-[1920px] h-[387px] left-0 top-0 absolute bg-orange-600 bg-opacity-80 rounded-bl-[80px] rounded-br-[80px] border" />
+  <div className="w-[1642px] h-[108px] left-[139px] top-0 absolute">
+    <div className="w-[1642px] h-[108px] left-0 top-0 absolute bg-indigo-700 rounded-bl-[40px] rounded-br-[40px] border" />
+    <img className="w-[254.55px] h-[86px] left-[93px] top-[5px] absolute border" src="https://via.placeholder.com/255x86" />
+    <img className="w-[52px] h-[52px] left-[53px] top-[33px] absolute border" src="https://via.placeholder.com/52x52" />
+    <div className="w-[127px] h-[81px] left-[1165px] top-[9px] absolute">
+      <img className="w-[81px] h-[81px] left-0 top-0 absolute border" src="https://via.placeholder.com/81x81" />
+      <div className="left-[41px] top-[23px] absolute text-white text-3xl font-semibold font-['Segoe UI']">IDR</div>
+      <img className="w-8 h-8 left-[95px] top-[27px] absolute rounded-[22px] border" src="https://via.placeholder.com/32x32" />
+    </div>
+    <div className="left-[1356px] top-[29px] absolute text-white text-3xl font-semibold font-['Segoe UI']">Log In</div>
+    <div className="w-[155px] h-[50px] left-[1456px] top-[23px] absolute bg-white rounded-[10px] border-2 border-white" />
+    <div className="left-[1478px] top-[29px] absolute text-indigo-700 text-3xl font-semibold font-['Segoe UI']">Register</div>
+  </div>
+  <div className="w-[1433px] h-[25px] left-[244px] top-[216px] absolute border border-neutral-500" />
+  <div className="w-[1200px] h-[25px] left-[244px] top-[216px] absolute bg-slate-400 border border-neutral-500" />
+  <div className="left-[623px] top-[260px] absolute text-black text-[40px] font-semibold font-['Segoe UI']">Sedang Mencari Penerbangan … 85%</div>
+  <div className="left-[139px] top-[390px] absolute text-black text-[35px] font-normal font-['Segoe UI']">Filter :</div>
+  <div className="w-[339px] h-[60px] left-[188px] top-[435px] absolute">
+    <div className="w-[339px] h-[60px] left-0 top-0 absolute bg-white rounded-[20px] border-2 border-black" />
+    <img className="w-10 h-10 left-[285px] top-[11px] absolute border" src="https://via.placeholder.com/40x40" />
+    <div className="left-[32px] top-[13px] absolute text-black text-[28px] font-normal font-['Segoe UI']">Harga Terendah</div>
+  </div>
+  <div className="w-[300px] h-[60px] left-[552px] top-[435px] absolute">
+    <div className="w-[300px] h-[60px] left-0 top-0 absolute bg-white rounded-[20px] border-2 border-black" />
+    <img className="w-10 h-10 left-[246px] top-[11px] absolute border" src="https://via.placeholder.com/40x40" />
+    <div className="left-[32px] top-[13px] absolute text-black text-[28px] font-normal font-['Segoe UI']">Maskapai</div>
+  </div>
+  <div className="w-[300px] h-[60px] left-[1202px] top-[435px] absolute">
+    <div className="w-[300px] h-[60px] left-0 top-0 absolute bg-white rounded-[20px] border-2 border-black" />
+    <img className="w-10 h-10 left-[246px] top-[11px] absolute border" src="https://via.placeholder.com/40x40" />
+    <div className="left-[32px] top-[13px] absolute text-black text-[28px] font-normal font-['Segoe UI']">Bagasi</div>
+  </div>
+  <div className="w-[300px] h-[60px] left-[877px] top-[435px] absolute">
+    <div className="w-[300px] h-[60px] left-0 top-0 absolute bg-white rounded-[20px] border-2 border-black" />
+    <img className="w-10 h-10 left-[246px] top-[11px] absolute border" src="https://via.placeholder.com/40x40" />
+    <div className="left-[32px] top-[13px] absolute text-black text-[28px] font-normal font-['Segoe UI']">Langsung</div>
+  </div>
+  <div className="w-[155px] h-[60px] left-[1527px] top-[435px] absolute">
+    <div className="w-[155px] h-[60px] left-0 top-0 absolute bg-indigo-700 rounded-[10px] border-2 border-white" />
+    <div className="left-[33px] top-[12px] absolute text-white text-3xl font-semibold font-['Segoe UI']">Search</div>
+  </div>
+  <img className="w-[70px] h-[70px] left-[1284px] top-[1823px] absolute rounded-[40px] border" src="https://via.placeholder.com/70x70" />
+  <div className="w-[1640px] h-[200px] left-[140px] top-[572px] absolute bg-gradient-to-br from-white via-gray-200 to-zinc-500 rounded-[45px] border border-indigo-700" />
+  <div className="w-[1640px] h-[200px] left-[140px] top-[849px] absolute bg-gradient-to-br from-white via-gray-200 to-zinc-500 rounded-[45px] border border-indigo-700" />
+  <div className="w-[1640px] h-[200px] left-[140px] top-[1114px] absolute bg-gradient-to-br from-white via-gray-200 to-zinc-500 rounded-[45px] border border-indigo-700" />
+  <div className="w-[1640px] h-[200px] left-[140px] top-[1391px] absolute bg-gradient-to-br from-white via-gray-200 to-zinc-500 rounded-[45px] border border-indigo-700" />
+  <div className="w-[1640px] h-[200px] left-[140px] top-[1656px] absolute bg-gradient-to-br from-white via-gray-200 to-zinc-500 rounded-[45px] border border-indigo-700" />
+  <div className="w-[1640px] h-[200px] left-[140px] top-[1933px] absolute bg-gradient-to-br from-white via-gray-200 to-zinc-500 rounded-[45px] border border-indigo-700" />
+  <div className="w-[1640px] h-[200px] left-[140px] top-[2173px] absolute bg-gradient-to-br from-white via-gray-200 to-zinc-500 rounded-[45px] border border-indigo-700" />
+  <div className="w-[945.93px] h-[315.75px] left-[487.50px] top-[2687.25px] absolute">
+    <div className="left-0 top-0 absolute text-black text-[28px] font-semibold font-['Segoe UI']">Contact Us</div>
+    <div className="w-[202px] h-[115px] left-0 top-[48px] absolute opacity-60 text-black text-xl font-normal font-['Segoe UI'] leading-[49px]">Customer Service<br/>Service Protection<br/>Feedback Situs Web</div>
+    <img className="w-[60px] h-[60.62px] left-[387.50px] top-[254.50px] absolute border" src="https://via.placeholder.com/60x61" />
+    <img className="w-[60px] h-[60px] left-[457.50px] top-[255.12px] absolute border" src="https://via.placeholder.com/60x60" />
+    <img className="w-[60px] h-[60.62px] left-[527.50px] top-[255.12px] absolute rounded-[30px] border" src="https://via.placeholder.com/60x61" />
+    <div className="left-[427.85px] top-0 absolute text-center text-black text-[28px] font-semibold font-['Segoe UI']">About Us</div>
+    <div className="w-[219px] h-[161px] left-[378px] top-[48px] absolute opacity-60 text-center text-black text-xl font-normal font-['Segoe UI'] leading-[49px]">About TiketQ<br/>News<br/>Resources and Policies<br/></div>
+    <div className="left-[785.93px] top-0 absolute text-right text-black text-[28px] font-semibold font-['Segoe UI']">Get The App</div>
+    <div className="w-[157px] h-[115px] left-[787px] top-[48px] absolute opacity-60 text-right text-black text-xl font-normal font-['Segoe UI'] leading-[49px]">Android App<br/>Iphone App<br/></div>
+  </div>
+  <div className="w-[1639px] h-[0px] left-[140.50px] top-[2660.75px] absolute opacity-60 border border-neutral-500"></div>
+  <div className="w-[1640px] h-[200px] left-[140px] top-[2438px] absolute bg-gradient-to-br from-white via-gray-200 to-zinc-500 rounded-[45px] border border-indigo-700" />
+</div>
+    </main>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,85 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="w-[1920px] h-[2193px] relative bg-white">
+  <div className="h-[174.50px] left-[140.50px] top-[2047.50px] absolute">
+    <div className="w-[1639px] h-[0px] left-0 top-0 absolute opacity-60 border border-neutral-500"></div>
+    <div className="w-[787px] h-[161px] left-[426.50px] top-[13.50px] absolute opacity-60 text-center text-black text-xl font-normal font-['Segoe UI'] leading-[49px]">Hak cipta © 2018 TiketQ.com Travel Pte. Ltd. All rights reserved<br/>PT. INITIKETQUE | Batam, Indonesia</div>
+  </div>
+  <div className="w-[1920px] h-[371px] left-0 top-0 absolute bg-orange-600 bg-opacity-80 rounded-bl-[80px] rounded-br-[80px] border" />
+  <div className="w-[1642px] h-[108px] left-[139px] top-0 absolute">
+    <div className="w-[1642px] h-[108px] left-0 top-0 absolute bg-indigo-700 rounded-bl-[40px] rounded-br-[40px] border" />
+    <img className="w-[254.55px] h-[86px] left-[93px] top-[5px] absolute border" src="img/favicon.ico" />
+    <img className="w-[52px] h-[52px] left-[53px] top-[33px] absolute border" src="https://via.placeholder.com/52x52" />
+    <div className="w-[127px] h-[81px] left-[1165px] top-[9px] absolute">
+      <img className="w-[81px] h-[81px] left-0 top-0 absolute border" src="https://via.placeholder.com/81x81" />
+      <div className="left-[41px] top-[23px] absolute text-white text-3xl font-semibold font-['Segoe UI']">IDR</div>
+      <img className="w-8 h-8 left-[95px] top-[27px] absolute rounded-[22px] border" src="https://via.placeholder.com/32x32" />
+    </div>
+    <div className="left-[1356px] top-[29px] absolute text-white text-3xl font-semibold font-['Segoe UI']">Log In</div>
+    <div className="w-[155px] h-[50px] left-[1456px] top-[23px] absolute bg-white rounded-[10px] border-2 border-white" />
+    <div className="left-[1478px] top-[29px] absolute text-indigo-700 text-3xl font-semibold font-['Segoe UI']">Register</div>
+  </div>
+  <div className="left-[200px] top-[427px] absolute text-black text-[35px] font-normal font-['Segoe UI']">Filter :</div>
+  <div className="w-[339px] h-[60px] left-[249px] top-[472px] absolute">
+    <div className="w-[339px] h-[60px] left-0 top-0 absolute bg-white rounded-[20px] border-2 border-black" />
+    <img className="w-10 h-10 left-[285px] top-[11px] absolute border" src="https://via.placeholder.com/40x40" />
+    <div className="left-[32px] top-[13px] absolute text-black text-[28px] font-normal font-['Segoe UI']">Harga Terendah</div>
+  </div>
+  <div className="w-[300px] h-[60px] left-[613px] top-[472px] absolute">
+    <div className="w-[300px] h-[60px] left-0 top-0 absolute bg-white rounded-[20px] border-2 border-black" />
+    <img className="w-10 h-10 left-[246px] top-[11px] absolute border" src="https://via.placeholder.com/40x40" />
+    <div className="left-[32px] top-[13px] absolute text-black text-[28px] font-normal font-['Segoe UI']">Maskapai</div>
+  </div>
+  <div className="w-[300px] h-[60px] left-[1263px] top-[472px] absolute">
+    <div className="w-[300px] h-[60px] left-0 top-0 absolute bg-white rounded-[20px] border-2 border-black" />
+    <img className="w-10 h-10 left-[246px] top-[11px] absolute border" src="https://via.placeholder.com/40x40" />
+    <div className="left-[32px] top-[13px] absolute text-black text-[28px] font-normal font-['Segoe UI']">Bagasi</div>
+  </div>
+  <div className="w-[300px] h-[60px] left-[938px] top-[472px] absolute">
+    <div className="w-[300px] h-[60px] left-0 top-0 absolute bg-white rounded-[20px] border-2 border-black" />
+    <img className="w-10 h-10 left-[246px] top-[11px] absolute border" src="https://via.placeholder.com/40x40" />
+    <div className="left-[32px] top-[13px] absolute text-black text-[28px] font-normal font-['Segoe UI']">Langsung</div>
+  </div>
+  <div className="w-[155px] h-[60px] left-[1588px] top-[472px] absolute">
+    <div className="w-[155px] h-[60px] left-0 top-0 absolute bg-indigo-700 rounded-[10px] border-2 border-white" />
+    <div className="left-[33px] top-[12px] absolute text-white text-3xl font-semibold font-['Segoe UI']">Search</div>
+  </div>
+  <div className="left-[484px] top-[1587px] absolute text-black text-[75px] font-semibold font-['Segoe UI']">Penerbangan Tidak Tersedia</div>
+  <div className="w-[853px] h-[100px] left-[534px] top-[1735px] absolute">
+    <div className="w-[853px] h-[100px] left-0 top-0 absolute">
+      <div className="w-[853px] h-[100px] left-0 top-0 absolute bg-indigo-700 rounded-[50px] border-2 border-white" />
+      <div className="left-[249px] top-[21px] absolute text-white text-[50px] font-semibold font-['Segoe UI']">Ganti Pencarian</div>
+    </div>
+    <img className="w-[70px] h-[70px] left-[765px] top-[15px] absolute rounded-[35px] border" src="https://via.placeholder.com/70x70" />
+  </div>
+  <div className="w-[1091.75px] h-[964.50px] left-[414.12px] top-[624.50px] absolute">
+    <div className="w-[883.50px] h-[964.50px] left-0 top-0 absolute">
+      <img className="w-[600px] h-[600px] left-[283.50px] top-0 absolute border" src="https://via.placeholder.com/600x600" />
+      <div className="w-[578.83px] h-[0px] left-[383px] top-[125.50px] absolute origin-top-left rotate-[131.43deg] border-4 border-black"></div>
+      <div className="w-[299.74px] h-[0px] left-[692px] top-[486.50px] absolute origin-top-left rotate-[129.59deg] border-4 border-black"></div>
+      <div className="w-[572.97px] h-[0px] left-[734px] top-[524.50px] absolute origin-top-left rotate-[129.83deg] border-4 border-black"></div>
+      <div className="w-[350.73px] h-[0px] left-[325.50px] top-[459.50px] absolute origin-top-left rotate-[132.40deg] border-4 border-black"></div>
+      <div className="w-[234.62px] h-[0px] left-[437.50px] top-[600px] absolute origin-top-left rotate-[131.03deg] border-4 border-black"></div>
+      <div className="w-[234.62px] h-[0px] left-[437.50px] top-[541px] absolute origin-top-left rotate-[131.03deg] border-4 border-black"></div>
+      <div className="w-[234.62px] h-[0px] left-[437.50px] top-[600px] absolute origin-top-left rotate-[131.03deg] border-4 border-black"></div>
+      <div className="w-[234.62px] h-[0px] left-[402.50px] top-[182px] absolute origin-top-left rotate-[131.03deg] border-4 border-black"></div>
+    </div>
+    <img className="w-64 h-64 left-[835.75px] top-[0.25px] absolute border" src="https://via.placeholder.com/256x256" />
+  </div>
+  <div className="w-[1559px] h-[202px] left-[181px] top-[131px] absolute bg-white rounded-[60px] border border-neutral-500" />
+  <div className="left-[238px] top-[162px] absolute text-black text-[45px] font-semibold font-['Segoe UI']">Batam (BTH)</div>
+  <img className="w-[75px] h-[75px] left-[510px] top-[151px] absolute border" src="https://via.placeholder.com/75x75" />
+  <div className="left-[601px] top-[165px] absolute text-black text-[45px] font-semibold font-['Segoe UI']">Bali (DPS)</div>
+  <div className="left-[236px] top-[230px] absolute text-neutral-500 text-[40px] font-normal font-['Segoe UI']">Sab, 19 Ags 2023  |  1 Penumpang  |  Premium Economy</div>
+  <img className="w-[100px] h-[100px] left-[1605px] top-[177px] absolute rounded-[50px] border" src="https://via.placeholder.com/100x100" />
+  <div className="w-[400px] h-[60px] left-[760px] top-[341px] absolute">
+    <div className="w-[400px] h-[60px] left-0 top-0 absolute bg-indigo-700 rounded-[20px] border" />
+    <div className="left-[128px] top-[12px] absolute text-white text-3xl font-semibold font-['Segoe UI']">Add Route</div>
+  </div>
+</div>
+    </main>
+  )
+}
